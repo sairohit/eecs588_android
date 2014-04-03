dc65db63aca837e2700910eb9920bb7e4741865b
sai <sai@email.com>
2014-04-03 11:51:39 -0400

    added http post request and bluetooth scanning. need to test...

---
+++ T3/src/eecs588/project/BTScanService.java
@@ -0,0 +1,107 @@
+package eecs588.project;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.app.IntentService;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+
+public class BTScanService extends IntentService {
+
+	private BluetoothAdapter btAdap; 
+	private boolean mScanning; 
+	private Handler mHandler; 
+	
+	// stop scanning after 20 seconds 
+	private static final long SCAN_PERIOD = 20000; 
+	
+	public BTScanService() {
+		super("BTScanService");
+	}
+
+	// this service will scan bluetooth every 5 minutes, capture the list of devices, and ship 'em off to the server
+	// we don't need to deal with broadcast receivers cuz we don't need to report to the main activity
+	// once we do our scan and post, the intent just stops itself
+	
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		final BluetoothManager btMan = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE); 
+        btAdap = btMan.getAdapter(); 
+		
+        scanDevices(true);
+		
+	}
+	
+	protected void scanDevices(final boolean enable) {
+		if (enable) {
+			//stops scanning after the predefined period
+			mHandler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					mScanning = false; 
+					btAdap.stopLeScan(bleScanCallback);
+				}
+			}, SCAN_PERIOD); 
+			
+			mScanning = true; 
+			btAdap.startLeScan(bleScanCallback); 
+		} 
+		else {
+			mScanning = false;
+			btAdap.stopLeScan(bleScanCallback);
+		}
+	}
+
+	private BluetoothAdapter.LeScanCallback bleScanCallback = 
+			new BluetoothAdapter.LeScanCallback() {
+				@Override
+				public void onLeScan(BluetoothDevice btDevice, int rssi, byte[] scanRecord) {
+					String macAddr = btDevice.getAddress(); 
+					String device = "android"; 
+					String passcode = "eecs588isalright"; 
+					Long ts = System.currentTimeMillis(); 
+					String timestamp = ts.toString(); 
+				
+					HttpClient httpClient = new DefaultHttpClient(); 
+					HttpPost httpPost = new HttpPost("https://track-dev.schultetwins.com/api/v1.0/spot"); 
+					
+					try {
+						List<NameValuePair> params = new ArrayList<NameValuePair>(7); 
+						params.add(new BasicNameValuePair("MAC", macAddr)); 
+						params.add(new BasicNameValuePair("RSSI", Integer.toString(rssi))); 
+						params.add(new BasicNameValuePair("timestamp", timestamp));
+						params.add(new BasicNameValuePair("device", device));
+						params.add(new BasicNameValuePair("passcode", passcode));
+						params.add(new BasicNameValuePair("latitude", "LATITUDE")); 
+						params.add(new BasicNameValuePair("longitude", "LONGITUDE")); 
+						httpPost.setEntity(new UrlEncodedFormEntity(params));
+						
+						//execute HTTP post
+						HttpResponse response = httpClient.execute(httpPost); 
+					} 
+					catch (ClientProtocolException ex) {						
+						//process the error 
+					}
+					catch (IOException ex) {						
+						//process the error 					
+					}
+									
+				}
+			};
+}
---
+++ T3/src/eecs588/project/MainActivity.java
@@ -0,0 +1,101 @@
+package eecs588.project;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	static final int REQUEST_ENABLE_BT = 1;  
+	
+	private BluetoothAdapter btAdap; 
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+        
+        final BluetoothManager btMan = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE); 
+        btAdap = btMan.getAdapter(); 
+        
+        //make sure BT is enabled
+        if (btAdap == null || !btAdap.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        
+        //at this point, bluetooth should be turned on
+        //create the intent, then pass it off to our IntentService
+        //do this every five minutes 
+        Intent myIntent = new Intent(this, BTScanService.class); 
+        startService(myIntent); 
+           
+        
+    }
+
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			if (resultCode != RESULT_OK) {
+				Toast.makeText(getApplicationContext(), "Please turn on Bluetooth before opening this app!", 
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+
+	}
+
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
